89596148a1822ed29668ed2289735e7b01b4dd0c
Ulan Seitkaliyev <ulanseitkaliyev@cmu.edu>
2022-05-17 22:08:32 +0600

    schedule

--- components/Schedule.tsx
+++ components/Schedule.tsx
@@ -1,8 +1,19 @@
 import React from "react";
-import { students, Student } from "../configuration";
+import schedule from "./schedule.json";
 
+const getTopic = ({ topic, breakDay, presentation, first, second }: any) => {
+  if (topic) return topic;
+  if (breakDay) return "Break, No lecture";
+  if (presentation){
+    return <>
+      <strong>Presentation 1: </strong><span className="text-xs">{first}</span>
+      <br/>
+      <strong>Presentation 2: </strong><span className="text-xs">{second}</span>
+    </>
+  }
+};
 
-const Schedule = ({points = {}}: any) => {
+const Schedule = () => {
   return (
     <div className="flex flex-col">
       <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -15,24 +26,33 @@ const Schedule = ({points = {}}: any) => {
                     scope="col"
                     className="text-sm font-bold text-gray-900 px-6 py-4 text-left"
                   >
-                    Name
+                    Date
                   </th>
                   <th
                     scope="col"
                     className="text-sm font-bold text-gray-900 px-6 py-4 text-left"
                   >
-                    Projected Total
+                    Day
+                  </th>
+                  <th
+                    scope="col"
+                    className="text-sm font-bold text-gray-900 px-6 py-4 text-left"
+                  >
+                    Topic
                   </th>
                 </tr>
               </thead>
               <tbody>
-                {students.map((student) => (
-                  <tr className="border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                {schedule.map((event) => (
+                  <tr key={event.date} className={(event.breakDay ? "schedule-bg-green-400" : "hover:bg-gray-100") + " border-b transition duration-300 ease-in-out"}>
                     <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                      <a href={student.link}>{student.name}</a>
+                      {event.date}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      {event.day}
                     </td>
                     <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      {points[student.link]}
+                      {getTopic(event)}
                     </td>
                   </tr>
                 ))}
