abbbc6d9924c839086410bf734f3975fdbe48ad9
Ulan Seitkaliyev <ulanseitkaliyev@cmu.edu>
2022-06-09 14:01:22 +0600

    update next config for future

--- next.config.js
+++ next.config.js
@@ -1,15 +1,24 @@
 // next.config.js
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.js"
+  themeConfig: "./theme.config.js",
 });
-const withTM = require("next-transpile-modules")([
-  "nextra-theme-docs"
-]);
+const withTM = require("next-transpile-modules")(["nextra-theme-docs"]);
+
+// const isProd = process.env.NODE_ENV === 'production'
+
+// module.exports = withTM(
+//   {...withNextra({
+//     reactStrictMode: true,
+//     trailingSlash: true
+//   }),
+//   assetPrefix: isProd ? 'https://web2.qatar.cmu.edu/~useitkal/07129' : '',
+//   }
+// );
 
 module.exports = withTM(
   withNextra({
     reactStrictMode: true,
-    trailingSlash: true
+    trailingSlash: true,
   })
 );
